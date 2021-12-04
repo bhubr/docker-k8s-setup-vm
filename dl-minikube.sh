8b055a1ba1bd17120ac92fad98167d1cec0c1b41
Benoit Hubert <benoithubert@gmail.com>
2021-12-04 18:35:02 +0100

    download script for minikube

---
+++ dl-minikube.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+curl -o minikube https://github.com/kubernetes/minikube/releases/download/v1.22.0/minikube-linux-amd64
+curl -o kubectl https://storage.googleapis.com/kubernetes-release/release/v1.22.0/bin/linux/amd64/kubectl
